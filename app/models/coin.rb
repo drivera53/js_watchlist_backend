1ee9092825a4885b0bc29454401c043b979654fc
Danny Rivera <samboard@hotmail.com>
2021-05-21 18:01:31 -0400

    Has many and has many through associations completed.

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -1,2 +1,4 @@
 class Coin < ApplicationRecord
+    has_many :watchlist_coins
+    has_many :watchlists, through: :watchlist_coins
 end
--- db/migrate/20210521214528_create_coins.rb
+++ db/migrate/20210521214528_create_coins.rb
@@ -1,7 +1,7 @@
 class CreateCoins < ActiveRecord::Migration[6.1]
   def change
     create_table :coins do |t|
-      t.string :coin_id
+      t.string :coin_api_id
       t.string :name
       t.string :symbol
 
